6add401be96cfddffce596abb943a5a9974b19c2
pxilips <58148159+pxilips@users.noreply.github.com>
2023-09-12 18:38:48 +0300

    Create variables.tf

---
+++ HW 19/Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "Enter Region"
+  default = "ca-central-1"
+}
+
+variable "access" {
+  description = "Enter your access key"
+  default = "key"
+}
+
+variable "secret" {
+  description = "Enter your secret key"
+  default = "key"
+}
